7c094e87b34e25a59d53bb93603fef470a147d05
actalexis <actalexis@gmail.com>
2015-02-23 23:09:56 -0500

    Revert "set up protractor conf.js and added a basic test to the suit"
    This reverts commit 7736fd80551b3146ab0075a31ab0b15700728321.

--- support/public/tests/unit tests/Testsuit1.js
+++ support/public/tests/unit tests/Testsuit1.js
@@ -1,16 +1,7 @@
 //sample jasmine test
-
 describe("A suite", function() {
   it("contains spec with an expectation", function() {
     expect(true).toBe(true);
   });
 });
-
-describe('check Title', function() {
-  it('should have a title', function() {
-    browser.get('http://localhost:3000/');
-    expect(browser.getTitle()).toEqual('Auralux Support and Feedback');
-  });
-});
-
 
